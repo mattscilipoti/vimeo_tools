a4ad4c2a5220b386e4aa21d8af6ef0bdb7d93164
Matt Scilipoti <matt@scilipoti.name>
2026-02-25 03:57:32 +0000

    overview: Add folder info

--- app/controllers/overview_controller.rb
+++ app/controllers/overview_controller.rb
@@ -10,17 +10,44 @@ class OverviewController < ApplicationController
   def fetch_vimeo_data
     return nil unless current_user.vimeo_access_token.present?
 
+    data = {}
+
     begin
       user = VimeoRuby::User.get_user(access_token: current_user.vimeo_access_token)
-      videos = user.uploaded_videos
-      {
-        name: user.name,
-        bio: user.bio,
-        videos_total: videos.videos.count,
-        profile_link: user.profile_link
-      }
+      data[:name] = user.name
+      data[:bio] = user.bio
+      data[:profile_link] = user.profile_link
     rescue => e
-      { error: "Failed to fetch Vimeo data: #{e.message}" }
+      data[:error_user] = "Failed to fetch user data: #{e.message}"
     end
+
+    begin
+      videos = VimeoRuby::User.get_user(access_token: current_user.vimeo_access_token).uploaded_videos
+      data[:videos_total] = videos.videos.count
+    rescue => e
+      data[:error_videos] = "Failed to fetch videos: #{e.message}"
+    end
+
+    begin
+      uri = URI("https://api.vimeo.com/me/projects")
+      request = Net::HTTP::Get.new(uri)
+      request["Authorization"] = "Bearer #{current_user.vimeo_access_token}"
+
+      response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+        http.request(request)
+      end
+
+      folders_response = JSON.parse(response.body)
+      data[:folders] = folders_response["data"].map do |folder|
+        {
+          name: folder["name"],
+          video_count: folder["metadata"]["connections"]["videos"]["total"]
+        }
+      end
+    rescue => e
+      data[:error_folders] = "Failed to fetch folders: #{e.message}"
+    end
+
+    data
   end
 end
